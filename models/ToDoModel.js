d79379e8170b5586e20932b01a7a8b9f1173edd8
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-10-23 12:34:00 +0530

    ToDoModel.js

--- models/ToDoModel.js
+++ models/ToDoModel.js
@@ -1,8 +1,10 @@
-const mongooose = require("mongoose");
+const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
     text:{
         type: String,
         required: true,
     }
-})
+});
+
+module.exports = mongoose.model("ToDo", todoSchema);
