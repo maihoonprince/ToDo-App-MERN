be801060922ebe920b4ab2deaa3a5c7807635bbe
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-10-23 12:34:56 +0530

    ToDoRoutes.js

--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -1,12 +1,10 @@
-const { Router } = require("express");
-
 const router = Router();
 
 const {getToDo, saveToDo, deleteToDo, updateToDo} = require("../controllers/ToDoController");
 
 router.get("/get-todo", getToDo);
 router.post("/save-todo", saveToDo);
-router.post("/dalate-todo", deleteToDo);
-router.post("update-todo", updateToDo);
+router.post("/delete-todo", deleteToDo);
+router.post("/update-todo", updateToDo);
 
 module.exports = router;
