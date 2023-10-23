806473a7b09ad66f12b85d02e0f5730c1ee77df5
PR!nce <108923246+maihoonprince@users.noreply.github.com>
2023-10-23 12:32:12 +0530

    server.js

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ require("dotenv").config();
 
 const cors = require("cors");
 
-const routes = require("./routes/ToDoRoutes");
+const routes = require("./routes/TodoRoute");
 
 const app = express();
+const PORT = process.env.PORT | 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
 app.use(routes);
@@ -17,11 +18,9 @@ app.use(routes);
 mongoose
 .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
 })
 .then(() => console.log("Mongodb Connected..."))
 .catch((err) => console.error(err));
 
-app.listen(6000, () => {
-    console.log("App listening on PORT 6000");
-});
+app.listen(PORT, () => console.log("Server Connected on port" + PORT));
